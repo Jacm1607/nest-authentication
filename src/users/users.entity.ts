e4a6beb9d36f33b630007085deb7f2a6fdc724f0
Jose Chavarria <chavarriaj84@gmail.com>
2023-04-27 19:51:15 -0400

    v1

---
+++ src/users/users.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  1000: string;
+
+  @Column()
+  2000: string;
+}
